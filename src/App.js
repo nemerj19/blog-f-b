5380801e09f1a7ee416d218f82c1f549626d395d
John Doe <johndoe@example.com>
2024-05-28 14:48:40 +0200

    Initial setup for [feature/bug]

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="#" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </nav>
+      </header>
+      <div className="entry">
+        <img
+          src="https://blog.wego.com/wp-content/uploads/shutterstock_2289237897.jpg"
+          alt=""
+        />
+        <h2>
+          Halal Holidays Germany: A Friendly Itinerary Through the Land of
+          Innovators and Visionaries
+        </h2>
+      </div>
+    </main>
+  );
+}
+
+export default App;
