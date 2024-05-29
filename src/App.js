524283e9c507ce5e5def6203bb4a07de576bbb24
John Doe <johndoe@example.com>
2024-05-29 13:05:48 +0200

    Initial commit: Set up project structure

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Post from "./components/Post";
+import Layout from "./components/Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header>
-      <a href="#" className="logo">
+      <Link to="/" className="logo">
         MyBlog
-      </a>
+      </Link>
       <nav>
-        <a href="#">Login</a>
-        <a href="#">Register</a>
+        <Link to="/login">Login</Link>
+        <Link to="register">Register</Link>
       </nav>
     </header>
   );
