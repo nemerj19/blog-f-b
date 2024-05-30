fd359f3d520acb48d0685e106747589441168f56
John Doe <johndoe@example.com>
2024-05-30 16:35:27 +0200

    Fix bug in data processing function

--- api/index.js
+++ api/index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
 const app = express();
+
+const saltRounds = "xshxsh82w822jsn";
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.connect(
+  "mongodb+srv://nemerj19:Sahar123@cluster0.qqqo4fo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestDate: { username, password } });
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const userDoc = await User.create({
+      username,
+      password: hashedPassword,
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 app.listen(4000);
